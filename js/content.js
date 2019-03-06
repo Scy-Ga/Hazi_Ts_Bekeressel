b6375ebfe0210f632226916bae534bc52d822d4c
Sci-Ga <gabor.varga87@hotmail.com>
2019-03-06 20:16:51 +0100

    Kész

--- js/content.js
+++ js/content.js
@@ -10,28 +10,95 @@ class Content {
         // res.write("<b>Fejlesztői környezet telepítésének leírása, forráskód GitHub repository:</b><br>");
         // res.write("<a href='https://github.com/nitslaszlo/JedlikTsTemplate' target='_blank'>" +
         //     "https://github.com/nitslaszlo/JedlikTsTemplate</a><br>");
-        res.write("<h3> A számok 1 - 10-ig Do - While ciklussal</h3><br> ");
-        let i = 1;
+        // Console.WriteLine("Legnagyobb közös osztó (LNKO) meghatározása:");
+        // Console.Write("a=");
+        // ulong a = ulong.Parse(Console.ReadLine());
+        // Console.Write("b=");
+        // ulong b = ulong.Parse(Console.ReadLine());
+        // while (a != b)
+        // {
+        //     if (a > b) a = a - b;
+        //     else b = b - a;
+        // }
+        //     Console.WriteLine("A két szám LNKO-ja: {0}", a);
+        //     Console.ReadKey();
+        let a1 = 10;
+        let b1 = 25;
+        res.write("<h3>Legnagyobb közös osztó (LNKO) meghatározása:</h3>");
+        res.write("<p>Az 'a' érték megadása: " +
+            " <input type='number' name='sorszam' style='font-family:Courier;" +
+            "font - size: inherit; background:Lightblue;'" +
+            `value='${a1}'><br>`);
+        res.write("<p>Az 'b' érték megadása: " +
+            " <input type='number' name='sorszam' style='font-family:Courier;" +
+            "font - size: inherit; background:Lightblue; '" +
+            `value='${b1}'><br><br>`);
+        while (a1 !== b1) {
+            if (a1 > b1)
+                a1 = a1 - b1;
+            else
+                b1 = b1 - a1;
+        }
+        res.write(`A két szám LNKO-ja: ${a1}`);
+        res.write("<br><br><br>");
+        // Console.WriteLine("Legnagyobb közös osztó (LNKO) meghatározása:");
+        // Console.Write("a=");
+        // ulong a = ulong.Parse(Console.ReadLine());
+        // Console.Write("b=");
+        // ulong b = ulong.Parse(Console.ReadLine());
+        // ulong m; //maradék
+        // do
+        // {
+        // m = a % b; //maradékos osztás
+        // a = b; //előző maradék
+        // b = m; //új maradék
+        // } while (m != 0);
+        // Console.WriteLine("A két szám LNKO-ja: {0}", a);
+        // Console.ReadKey();
+        res.write("<h3>Legnagyobb közös osztó (LNKO) meghatározása:</h3>");
+        let a2 = 12;
+        let b2 = 25;
+        let m;
+        res.write("<p>Az 'a' érték megadása: " +
+            " <input type='number' name='sorszam' style='font-family:Courier;" +
+            "font - size: inherit; background:Lightblue;'" +
+            `value='${a2}'><br>`);
+        res.write("<p>Az 'b' érték megadása: " +
+            " <input type='number' name='sorszam' style='font-family:Courier;" +
+            "font - size: inherit; background:Lightblue; '" +
+            `value='${b2}'><br><br>`);
         do {
-            res.write(i + `,`);
-            i++;
-        } while (i <= 10);
-        res.write("<h3> A számok 1 - 10-ig While ciklussal</h3><br> ");
-        var f = 1;
-        while (f <= 10) {
-            res.write(f + `,`);
-            f++;
+            m = a2 % b2;
+            a2 = b2;
+            b2 = m;
+        } while (m !== 0);
+        res.write(`A két szám LNKO-ja: ${a2}`);
+        res.write("<br><br><br>");
+        // Console.WriteLine("Faktoriális meghatározása");
+        // Console.Write("n=");
+        // byte n = byte.Parse(Console.ReadLine());
+        // Console.Write("{0}! = 1",n);
+        // ulong faktor = 1;
+        // for (byte i = 2; i <= n; i++)
+        // {
+        // Console.Write(" * {0}", i);
+        // faktor = faktor * i;
+        // }
+        // Console.WriteLine(" = {0}", faktor);
+        // Console.ReadKey();
+        res.write("<h3>Faktoriális meghatározása:</h3>");
+        let n = 5;
+        let faktor = 1;
+        res.write("<p>Faktoriális meghatározása: " +
+            " <input type='number' name='sorszam' style='font-family:Courier;" +
+            "font - size: inherit; background:Lightblue;'" +
+            `value='${n}'><br><br>`);
+        res.write(n + "! = 1");
+        for (let i = 2; i <= n; i++) {
+            res.write(" * " + i);
+            faktor = faktor * i;
         }
-        res.write("<h3> A számok 1 - 10-ig For ciklussal</h3><br> ");
-        for (let g = 1; g <= 10; g++) {
-            res.write(g + `,`);
-        }
-        res.write("<br><br>");
-        // tslint:disable-next-line: max-line-length
-        // tslint:disable-next-line: align
-        res.write("<img height = 100 src = https://cdn.newsapi.com.au/image/v1/baa78e55168b70c08147f44f0d66cd0e?width=1024></img>");
-        res.write("<br><br>");
-        res.write("<img height = 100 src = ./a></img>");
+        res.write(" = " + faktor);
         res.end();
     }
 }
--- ts/content.ts
+++ ts/content.ts
@@ -13,7 +13,126 @@ export default class Content {
         // res.write("<a href='https://github.com/nitslaszlo/JedlikTsTemplate' target='_blank'>" +
         //     "https://github.com/nitslaszlo/JedlikTsTemplate</a><br>");
 
-        
+
+
+        // Console.WriteLine("Legnagyobb közös osztó (LNKO) meghatározása:");
+        // Console.Write("a=");
+        // ulong a = ulong.Parse(Console.ReadLine());
+        // Console.Write("b=");
+        // ulong b = ulong.Parse(Console.ReadLine());
+        // while (a != b)
+        // {
+        //     if (a > b) a = a - b;
+        //     else b = b - a;
+        // }
+        //     Console.WriteLine("A két szám LNKO-ja: {0}", a);
+        //     Console.ReadKey();
+
+
+
+        let a1: number = 10;
+        let b1: number = 25;
+        res.write("<h3>Legnagyobb közös osztó (LNKO) meghatározása:</h3>");
+        res.write("<p>Az 'a' érték megadása: " +
+            " <input type='number' name='sorszam' style='font-family:Courier;" +
+            "font - size: inherit; background:Lightblue;'" +
+            `value='${a1}'><br>`);
+
+        res.write("<p>Az 'b' érték megadása: " +
+            " <input type='number' name='sorszam' style='font-family:Courier;" +
+            "font - size: inherit; background:Lightblue; '" +
+            `value='${b1}'><br><br>`);
+
+
+        while (a1 !== b1) {
+
+
+            if (a1 > b1) a1 = a1 - b1;
+            else b1 = b1 - a1;
+
+        }
+
+        res.write(`A két szám LNKO-ja: ${a1}`);
+
+        res.write("<br><br><br>");
+
+        // Console.WriteLine("Legnagyobb közös osztó (LNKO) meghatározása:");
+        // Console.Write("a=");
+        // ulong a = ulong.Parse(Console.ReadLine());
+        // Console.Write("b=");
+        // ulong b = ulong.Parse(Console.ReadLine());
+        // ulong m; //maradék
+        // do
+        // {
+        // m = a % b; //maradékos osztás
+        // a = b; //előző maradék
+        // b = m; //új maradék
+        // } while (m != 0);
+        // Console.WriteLine("A két szám LNKO-ja: {0}", a);
+        // Console.ReadKey();
+
+
+        res.write("<h3>Legnagyobb közös osztó (LNKO) meghatározása:</h3>");
+
+        let a2: number = 12;
+        let b2: number = 25;
+        let m: number;
+
+        res.write("<p>Az 'a' érték megadása: " +
+            " <input type='number' name='sorszam' style='font-family:Courier;" +
+            "font - size: inherit; background:Lightblue;'" +
+            `value='${a2}'><br>`);
+
+        res.write("<p>Az 'b' érték megadása: " +
+            " <input type='number' name='sorszam' style='font-family:Courier;" +
+            "font - size: inherit; background:Lightblue; '" +
+            `value='${b2}'><br><br>`);
+
+        do {
+            m = a2 % b2;
+            a2 = b2;
+            b2 = m;
+
+        } while (m !== 0);
+
+        res.write(`A két szám LNKO-ja: ${a2}`);
+
+        res.write("<br><br><br>");
+
+
+        // Console.WriteLine("Faktoriális meghatározása");
+        // Console.Write("n=");
+        // byte n = byte.Parse(Console.ReadLine());
+        // Console.Write("{0}! = 1",n);
+        // ulong faktor = 1;
+        // for (byte i = 2; i <= n; i++)
+        // {
+        // Console.Write(" * {0}", i);
+        // faktor = faktor * i;
+        // }
+        // Console.WriteLine(" = {0}", faktor);
+        // Console.ReadKey();
+
+
+        res.write("<h3>Faktoriális meghatározása:</h3>");
+
+        let n: number = 5;
+        let faktor: number = 1;
+        res.write("<p>Faktoriális meghatározása: " +
+            " <input type='number' name='sorszam' style='font-family:Courier;" +
+            "font - size: inherit; background:Lightblue;'" +
+            `value='${n}'><br><br>`);
+
+        res.write(n + "! = 1");
+
+        for (let i = 2; i <= n; i++) {
+            res.write(" * " + i);
+            faktor = faktor * i;
+        }
+
+        res.write(" = " + faktor);
+
+        res.end();
     }
 }
 
